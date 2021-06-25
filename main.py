d1e8c2f90ac111eabaa5c7a353484957042b0c97
SlyJackal <slyjackal4@gmail.com>
2021-06-25 17:07:23 +0500

    Show. Main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from re import split
+Download git
+import git
+git.Git("/home/slyjackal/gitlab/clone").clone("https://github.com/SlyJackal/piska-bot.git")
+Get all messages from commits
+from git import Repo
+repo = Repo('/home/slyjackal/gitlab/clone/piska-bot')
+commits = list(repo.iter_commits('master'))
+how_many=(len(commits))
+print('Всего коммитов:', how_many)
+for commit in commits:
+    a=[commit.message]
+    print(a)
