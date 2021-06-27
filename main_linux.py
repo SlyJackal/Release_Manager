5c3cd1fb0b812bb920e632a425ba041292818d5b
SlyJackal <slyjackal4@gmail.com>
2021-06-27 21:26:25 +0500

    Some fix

--- main_linux.py
+++ main_linux.py
@@ -1,12 +1,10 @@
 from re import split
 #Download git
 import git
-git.Git("/home/slyjackal/gitlab/clone").clone("https://github.com/SlyJackal/piska-bot.git")
+git.Git("/home/slyjackal/Git").clone("https://github.com/SlyJackal/piska-bot.git")
 #Get all messages from commits
-repo = git.Repo('/home/slyjackal/gitlab/clone/piska-bot')
+repo = git.Repo('/home/slyjackal/Git/piska-bot')
 commits = repo.iter_commits('master')
-how_many=(len(commits))
-print('Всего коммитов:', how_many)
 for commit in commits:
-    a=[commit.message]
+    a=(commit.message)
     print(a)
