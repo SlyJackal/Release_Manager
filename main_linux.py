b2ceebdf6ddf1c731df8daabce9479934e8b3ed1
SlyJackal <slyjackal4@gmail.com>
2021-06-28 21:14:25 +0500

    in progress

--- main_linux.py
+++ main_linux.py
@@ -1,10 +1,22 @@
-from re import split
+import re
+#re.split and re.sub
 #Download git
 import git
-git.Git("/home/slyjackal/Git").clone("https://github.com/SlyJackal/piska-bot.git")
+#git.Git("/home/slyjackal/Git").clone("https://github.com/SlyJackal/piska-bot.git")
 #Get all messages from commits
 repo = git.Repo('/home/slyjackal/Git/piska-bot')
 commits = repo.iter_commits('master')
+
+git_list=[]
 for commit in commits:
-    a=(commit.message)
-    print(a)
+    git_list.append(commit.message)
+   
+print(git_list)
+
+#Find all numbers
+#find from 4 to 5 numbers \d{4,5}
+#task_list = re.search(r'\d{4,5}', r=a)
+#task_list = re.search(r'\b[горь]\, git_list)
+#print (task_list.group(0))
+ if re.search(r'\d{4,5}\d', git_list) is not None:
+        print('I find')
