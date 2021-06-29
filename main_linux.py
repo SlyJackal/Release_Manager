04cc3d51c6e246d2c9299b5ae400f28bc40f503b
SlyJackal <slyjackal4@gmail.com>
2021-06-29 14:36:21 +0500

    pre Alfa done

--- main_linux.py
+++ main_linux.py
@@ -6,10 +6,18 @@ import re
 repo = git.Repo('/home/slyjackal/Git/piska-bot')
 commits = repo.iter_commits('master')
 
-git_list=''
+git_list_str=''
 for commit in commits:
-    git_list += commit.message
+    git_list_str += commit.message
 
-result = re.findall(r'\d{4,5}', git_list)
-print(result)
+git_list = re.findall(r'\d{4,5}', git_list_str)
+#print(git_list)
+
+
+jira_list = ['1234', '12345', '12345', '4444', '4444', '12345', '1234', '0000']
+#Check lists
+res = [x for x in git_list + jira_list if x not in jira_list]
+print('В Git нет следующих доработок:', res)
+
+
 
