622b3e0c63a6a956329083ee7ddddbaa685f498f
SlyJackal <slyjackal4@gmail.com>
2021-06-30 00:09:40 +0500

    Add Jira

--- main_linux.py
+++ main_linux.py
@@ -1,23 +1,40 @@
-#Download git
-import git
-git.Git("/home/slyjackal/Git").clone("https://github.com/SlyJackal/piska-bot.git")
-#Get all messages from commits
-import re
-repo = git.Repo('/home/slyjackal/Git/piska-bot')
-commits = repo.iter_commits('master')
 
+#Get data
+print('Введите куда клонировать Git, пример: /home/slyjackal/Git')
+clone_in=input()
+print('Введите адрес репозитория Git, пример: https://github.com/SlyJackal/piska-bot.git')
+clone_from=input()
+print('Введите название репозитория, пример: piska-bot')
+git_name=input()
+print('Введите название ветки, пример: master')
+fork_name=input()
+git.Git(clone_in).clone(clone_from)
+
+#Download git and get all messages from commits
+import git
+repo = git.Repo('/'.join(str(x) for x in [clone_in,git_name]))
+commits = repo.iter_commits(fork_name)
 git_list_str=''
 for commit in commits:
-    git_list_str += commit.message
+   git_list_str += commit.message
 
+#Find numbers of tasks
+import re
 git_list = re.findall(r'\d{4,5}', git_list_str)
-#print(git_list)
 
+#Get tasks from jira
+from jira import JIRA
+from jira.client import JiraCookieAuth
+print('Введите логин от Jira')
+login=input()
+print('Введите пароль от Jira')
+password=input()
+auth_jira = JIRA(basic_auth=(login, password))
+print('Введите JQL запрос для выбора задач')
+JQL=input()
+jira_list = [JiraCookieAuth.search_issues(JQL)]
+#jira_list = ['1234', '12345', '12345', '4444', '4444', '12345', '1234', '0000']
 
-jira_list = ['1234', '12345', '12345', '4444', '4444', '12345', '1234', '0000']
 #Check lists
 res = [x for x in git_list + jira_list if x not in jira_list]
-print('В Git нет следующих доработок:', res)
-
-
-
+print('В Git нет следующих доработок:', res
