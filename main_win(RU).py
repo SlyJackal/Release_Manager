1b1584cc8eec981366c826a680631f627516dda8
SlyJackal <slyjackal4@gmail.com>
2021-08-10 13:30:04 +0500

    1. Add Ru version. 2. Delete row: from jira.resources import Issue

--- main_win(RU).py
+++ main_win(RU).py
@@ -1,5 +1,4 @@
-#Get data
-"""
+#Данные о сборке
 print('Введите куда клонировать Git, пример: D:\Git')
 clone_in=input()
 print('Введите адрес репозитория Git, пример: https://github.com/SlyJackal/piska-bot.git')
@@ -8,56 +7,49 @@ print('Введите название репозитория, пример: pis
 git_name=input()
 print('Введите название ветки, пример: master')
 fork_name=input()
-"""
-#Test data
-clone_in='/home/slyjackal/Git'
-#relative path '.\'
-clone_from='https://github.com/SlyJackal/piska-bot.git'
-git_name='piska-bot'
-fork_name='master'
 
-
-#Download git and get all messages from commits
+#Скачать репозиторий
 import git, os
+from jira.resources import Issue
 if not os.path.isdir(clone_in + '/' + git_name):
    git.Git(clone_in).clone(clone_from)
+
+#Получить сообщения из коммитов
 repo = git.Repo('/'.join(str(x) for x in [clone_in,git_name]))
 commits = repo.iter_commits(fork_name)
 git_list_str=''
 for commit in commits:
    git_list_str += commit.message
 
-#Find numbers of tasks
+#Найти номера всех задач
 import re
 git_list = re.findall(r'\d{4,5}', git_list_str)
 
-#Get tasks from jira
+#Подключение модуля Jira
 from jira import JIRA
 from jira.client import JiraCookieAuth
-#Acces to Jira
+#Получить доступ в Jira
 print('Введите логин от Jira')
-login='slyjackal4@gmail.com'
-
-#By password
-#print('Введите пароль от Jira')
-#password=input()
-
-#By token
+login=input()
+#Через токен
 print('Введите ваш токен')
 token=input()
-auth_jira = JIRA('https://team-1602178802459.atlassian.net', basic_auth=(login, token))
-print('Введите JQL запрос для выбора задач')
-jql_string='project = AHEBURG order by created DESC'
+'''#Через пароль
+print('Введите пароль от Jira')
+password=input()'''
+
+#Подключение к нашей Jira
+jira_host='https://team-1602178802459.atlassian.net' '''Установить свой адрес'''
+auth_jira = JIRA(jira_host, basic_auth=(login, token))
+
+#Запрос списка задач у пользователя
+print('Введите JQL запрос для выбора задач. Пример: project = AHEBURG order by created DESC')
+jql_string=input()
 jira_list = auth_jira.search_issues(jql_string)
 issues_list=[]
 for issue in jira_list:
   issues_list.append(issue.key.split('-')[1])
-print('Jira list:')
-print(issues_list)
-print('Git list:')
-print(git_list)
 
-
-#Check lists
+#Проверка списка задач в Git
 res = [x for x in git_list + issues_list if x not in git_list]
 print('В Git нет следующих доработок:', res)
